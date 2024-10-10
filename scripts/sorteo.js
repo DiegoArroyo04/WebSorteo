d3ddf2e78143f0ed42f19c704354b549d2d35377
DiegoArroyo04 <diegoarroyogonzalez04@gmail.com>
2024-10-10 12:46:47 +0200

    correo funcional

--- scripts/sorteo.js
+++ scripts/sorteo.js
@@ -1,14 +1,29 @@
 const modal = document.getElementById("modal");
 const botonParticipar = document.getElementById("participar");
+const formulario = document.getElementById("formulario");
 
-botonParticipar.addEventListener("click", function () {
+formulario.addEventListener("submit", function (event) {
+    //MODAL CONFIRMAR PARTICIPACION
     modal.style.display = "block";
     //REGISTRAR EN BASE DE DATOS
 
 
-    //MODAL CONFIRMAR PARTICIPACION
+
 
     //MANDAR CORREO CONFIRMACION
+    event.preventDefault();
+    const serviceID = 'service_dy20kyj';
+    const templateID = 'template_pz76sxi';
+
+    emailjs.sendForm(serviceID, templateID, this)
+        .then(() => {
+
+            alert('Sent!');
+        }, (err) => {
+
+            alert(JSON.stringify(err));
+        });
+
 });
 
 //EVENTO PARA CERRAR EL MODAL AL PULSAR FUERA
